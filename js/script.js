7975fbe3929ef0d795cf2da7e8946d8ae7268c43
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-04-05 01:23:33 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lamees Hemdan
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+// This function calculates the area of a trapezoid
+
+function calculateVolume () {
+ // input
+  const length = parseFloat(document.getElementById('length').value)
+  const width = parseFloat(document.getElementById('width').value)
+  const height = parseFloat(document.getElementById('height').value)
+
+
+ // process
+  const volume = (length * height * width) / 3
+
+
+ // output
+  document.getElementById('volume').innerHTML = 'The Volume is ' + volume + ' mm³'
+}
